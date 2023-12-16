e351d9a3bc06cb917a776d2ce17acd0cd093c573
Preeti biswas <preeti.biswas@brovitech.com>
2023-12-16 12:35:00 +0530

    completed-feed component

--- src/component/Feed.jsx
+++ src/component/Feed.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState,useEffect } from 'react'
 import { Stack,Box,Typography } from '@mui/material'
 import Sidebar from './Sidebar'
+import Video from './Video'
 
 const Feed = () => {
   return (
@@ -9,10 +10,16 @@ const Feed = () => {
 
         <Box sx={{height:{sx:'auto',md:'92vh'},borderRight:'1px solid #3d3d3d',px:{sx:0,md:2}}}>
             <Sidebar/>
-            
+
             <Typography variant='body2'className='copyright' sx={{color:'#fff',mt:'1.5px'}}>
                 copyright 2022 jsm media
             </Typography>
+        </Box>
+        <Box>
+            <Typography sx={{color:'#fff'}} p={2} variant='h4' fontWeight='bold'>New <span style={{color:'#F31503'}}>Videos</span>
+                
+            </Typography>
+            <Video/>
         </Box>
     </Stack>
   )
