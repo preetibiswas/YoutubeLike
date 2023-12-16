a1e69473f873250b628ab03f68e06de9abb7bc27
Preeti biswas <preeti.biswas@brovitech.com>
2023-12-16 16:29:36 +0530

    checkuser

--- src/component/Video.jsx
+++ src/component/Video.jsx
@@ -2,7 +2,7 @@ import { Box } from '@mui/material'
 import React from 'react'
 
 const Video = ({videos}) => {
-    console.log(videos)
+    console.log('video',videos)
   return (
     <Box sx={{color:'#fff'}}>Video</Box>
   )
